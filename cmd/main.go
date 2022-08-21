2d0b75bf94c1d72c824734fb06ba1459efb2b27b
SubochevaValeriya <subocheva.valeria@gmail.com>
2022-08-21 22:27:39 +0300

    f

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,28 @@ import (
 	"github.com/SubochevaValeriya/microservice-balance/pkg/handler"
 	"github.com/SubochevaValeriya/microservice-balance/pkg/repository"
 	"github.com/SubochevaValeriya/microservice-balance/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing congigs: %s", err.Error())
+	}
+
 	// dependency injection
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(balance.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error ccured while running http server: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
