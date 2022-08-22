f66604b5d1abb2760724ec76e7c9412bf7dbb1a8
SubochevaValeriya <subocheva.valeria@gmail.com>
2022-08-23 00:23:02 +0300

    postgres

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,11 @@ import (
 	"github.com/SubochevaValeriya/microservice-balance/pkg/handler"
 	"github.com/SubochevaValeriya/microservice-balance/pkg/repository"
 	"github.com/SubochevaValeriya/microservice-balance/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
@@ -14,8 +17,25 @@ func main() {
 		log.Fatalf("error initializing congigs: %s", err.Error())
 	}
 
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	//sudo docker run --name=balance -e POSTGRES_PASSWORD='qwerty' -p 5432:5432 -d --rm postgres
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to inititalize db: %s", err.Error())
+	}
+
 	// dependency injection
-	repos := repository.NewRepository()
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(balance.Server)
