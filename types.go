a583f3d0710f4a2ae8be20872b3bdac958541e4c
SubochevaValeriya <subocheva.valeria@gmail.com>
2022-08-26 19:13:30 +0300

    ccy convertion in progress

--- types.go
+++ types.go
@@ -20,6 +20,22 @@ type Transactions struct {
 	Date time.Time `json:"transaction_date" db:"transaction_date"`
 }
 
+type ExhangeRates struct {
+	Date       string `json:"date"`
+	Historical string `json:"historical"`
+	Info       struct {
+		Rate      float64 `json:"rate"`
+		Timestamp int     `json:"timestamp"`
+	} `json:"info"`
+	Query struct {
+		Amount int    `json:"amount"`
+		From   string `json:"from"`
+		To     string `json:"to"`
+	} `json:"query"`
+	Result  float64 `json:"result"`
+	Success bool    `json:"success"`
+}
+
 func main() {
 }
 
